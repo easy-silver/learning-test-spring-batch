e819595382add1acb78dfc22584cdc3cbff1aac1
이지은/파트너포탈팀 <jieun2.lee@musinsa.com>
2023-03-23 20:40:36 +0900

    Initial commit

---
+++ src/main/java/com/example/learningtestspringbatch/LearningTestSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningtestspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTestSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningTestSpringBatchApplication.class, args);
+    }
+
+}
