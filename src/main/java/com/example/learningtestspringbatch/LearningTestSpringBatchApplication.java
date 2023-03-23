5bf32069939808d5c4c9e63bb17eeafb38dacbb1
Timo <developer.timo@gmail.com>
2023-03-23 21:09:15 +0900

    feat: Hello World Job

--- src/main/java/com/example/learningtestspringbatch/LearningTestSpringBatchApplication.java
+++ src/main/java/com/example/learningtestspringbatch/LearningTestSpringBatchApplication.java
@@ -1,10 +1,44 @@
 package com.example.learningtestspringbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class LearningTestSpringBatchApplication {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public LearningTestSpringBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step step() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LearningTestSpringBatchApplication.class, args);
